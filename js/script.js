02025bc44fde1765b24d9748561df0012caec754
ninochakvetadze. <nino.chakvetadze28b@geolab.edu.ge>
2026-02-12 22:33:59 +0400

    added responsive styles

--- js/script.js
+++ js/script.js
@@ -2,12 +2,20 @@ let offsetX = 0;
 let offsetY = 0;
 
 function moveNo() {
-    const maxMove = 300;
+    const maxMove = window.innerWidth < 600 ? 120 : 300;
+
     offsetX = (Math.random() - 0.5) * maxMove;
     offsetY = (Math.random() - 0.5) * maxMove;
 
     const noBtn = document.querySelector(".no");
     noBtn.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+}
+
+function noClick() {
+    if (window.innerWidth < 600) {
+        moveNo();
+        document.getElementById("response").innerText = " კარგად დაფიქრდი... 😏";
+    }
 }
 
 function yesClick() {
@@ -18,13 +26,15 @@ function yesClick() {
 function createHearts() {
     const card = document.querySelector(".card");
 
+    const range = window.innerWidth < 600 ? 300 : 800;
+
     for (let i = 0; i < 125; i++) {
         const heart = document.createElement("div");
         heart.className = "heart";
         heart.innerText = "❤️";
 
-        const x = (Math.random() - 0.5) * 800 + "px";
-        const y = (Math.random() - 0.5) * 800 + "px";
+        const x = (Math.random() - 0.5) * range + "px";
+        const y = (Math.random() - 0.5) * range + "px";
 
         heart.style.setProperty("--x", x);
         heart.style.setProperty("--y", y);
@@ -36,4 +46,5 @@ function createHearts() {
         setTimeout(() => heart.remove(), 1500);
     }
 }
+
 
